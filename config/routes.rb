0f1be8a6f042a31636b2f5708377973c4e880961
saganmon <fevers24@gmail.com>
2020-02-17 14:09:14 +0900

    changed Sells#index to #new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'mypages#index'
 
   resources :homes, only: [:index]
-  resources :sells, only: [:index, :new, :show]
+  resources :sells, only: [:new, :show]
 end
