b1e079e980f85be897939171ea69cf5417c36517
saganmon <fevers24@gmail.com>
2020-02-18 21:42:20 +0900

    tweat

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   root 'homes#index'
 
   resources :mypages, only: [:index]
-  resources :sells, only: [:new, :show] 
+  resources :sells, only: [:new, :create, :show] 
   resources :buys, only: [:new, :create]
   resources :homes, only: [:index]
   
