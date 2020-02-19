8120c739c7e8acbc8faa90d90d997b19de71781e
yamanekei <yamanekei@icroud.com>
2020-02-19 20:10:48 +0900

    修正済

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,9 +2,6 @@ class HomesController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc).limit(3)
-    # @products = Product.all.order(created_at: :desc).limit(3)
-
-
   end
   
 end
