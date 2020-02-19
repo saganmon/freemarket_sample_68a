16f28084e393e4e3360f710497ace948b2574fe9
saganmon <fevers24@gmail.com>
2020-02-19 19:54:13 +0900

    testing

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -3,6 +3,7 @@ class SellsController < ApplicationController
   def new
     @sell = Product.new
     @image = Image.new
+    @categories = Category.all
   end
 
   def create
@@ -12,6 +13,14 @@ class SellsController < ApplicationController
 
   def show
 
+  end
+
+  def edit
+    @child_categories = Category.where(ancestry: params[:keyword])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   private
