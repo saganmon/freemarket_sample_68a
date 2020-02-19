cb1642936f99fd707ba3306de783e823d8fd8812
yamanekei <yamanekei@icroud.com>
2020-02-19 19:44:38 +0900

    【サーバサイド】商品一覧表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,10 @@
 class HomesController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.all.order(created_at: :desc).limit(3)
+    # @products = Product.all.order(created_at: :desc).limit(3)
+
+
   end
   
 end
