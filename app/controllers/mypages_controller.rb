b60e54b7f979fe675a5488de0a5e88395a9a4ac9
saganmon <fevers24@gmail.com>
2020-02-28 12:35:44 +0900

    ititial implementation

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,6 +1,8 @@
 class MypagesController < ApplicationController
+  add_breadcrumb "フリマ", :root_path
+  add_breadcrumb "マイページ", :mypages_path
 
   def index
-    
+    add_breadcrumb "#{current_user.nickname}さん", mypages_path
   end
 end
