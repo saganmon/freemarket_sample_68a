78aff37378dd25d5830355d25596c43ee6af3149
saganmon <fevers24@gmail.com>
2020-03-02 10:19:40 +0900

    defined variable and used

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,21 @@
 $(function () {
+  function buildHTML(product) {
+    var html =
+      `<div class="search-main__right-contents__images__list">
+        <div class="search-main__right-contents__images__list__image">
+          <img width="220px" height="150px" src="${product.image}">
+        </div>
+        <div class="search-main__right-contents__images__list__name">
+          ${product.name}
+        </div>
+        <div class="search-main__right-contents__images__list__price">
+          ${product.price}
+        </div>
+      </div>`
+    return html;
+  };
+
+
   $(".search-main__left-contents__sort-selector__box").on("change", function() {
     var keyword = $(".search-main__right-contents__keyword").html()
     var num = $(this).val();
@@ -9,22 +26,13 @@ $(function () {
       dataType: 'json'
     })
     .done(function (products) {
-      $(".search-main__right-contents__images").empty();
-      $(products).each(function (i, product) {
-        $(".search-main__right-contents__images").append(
-          `<div class="search-main__right-contents__images__list">
-            <div class="search-main__right-contents__images__list__image">
-              <img width="220px" height="150px" src="${product.image}">
-            </div>
-            <div class="search-main__right-contents__images__list__name">
-              ${product.name}
-            </div>
-            <div class="search-main__right-contents__images__list__price">
-              ${product.price}
-            </div>
-          </div>`
-        );
-      });
+      if (products.length !== 0) {
+        var insertHTML = '';
+        $(".search-main__right-contents__images").empty();
+        $(products).each(function (i, product) {
+          insertHTML += buildHTML(product);
+        });
+      }
     })
     .fail(function () {
       alert('検索結果の取得に失敗しました');
