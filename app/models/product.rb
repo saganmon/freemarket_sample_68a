1962981d0289faf33e7b82146d231a8c6065bfe4
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-03-05 15:04:04 +0900

    ProductModel_update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to  :user
-  has_many    :images, dependent: :delete_all
+  has_many    :images, dependent: :destroy
   belongs_to  :category
   belongs_to  :shipping
   belongs_to  :brand, optional: true
