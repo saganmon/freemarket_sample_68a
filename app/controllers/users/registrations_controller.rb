be70ba66dc249e8c335e995ed10a5e9fe0d1accd
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-03-06 15:15:42 +0900

    最終チェック

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(sign_up_params)
-    binding.pry
     unless @user.valid?
       redirect_to new_user_registration_path, alert: '入力内容を確認してください'
       return
